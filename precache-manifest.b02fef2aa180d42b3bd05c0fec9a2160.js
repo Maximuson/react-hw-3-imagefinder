ff322098b0ee737d0d8ac33699751d3e7098dcee
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-03-09 17:25:25 +0200

    Updates

---
+++ precache-manifest.b02fef2aa180d42b3bd05c0fec9a2160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea6ecb3734c459142503c9afb45c5e20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64e9285f4cd364f0ecc9",
+    "url": "/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "1c1f7045deb67a5708eb",
+    "url": "/static/css/main.1ed04716.chunk.css"
+  },
+  {
+    "revision": "64e9285f4cd364f0ecc9",
+    "url": "/static/js/2.19310db2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.19310db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1f7045deb67a5708eb",
+    "url": "/static/js/main.9f95d92f.chunk.js"
+  },
+  {
+    "revision": "023c7ca359bd2113d980",
+    "url": "/static/js/runtime-main.bb878716.js"
+  }
+]);
