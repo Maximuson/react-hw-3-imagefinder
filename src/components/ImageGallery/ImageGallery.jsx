1e7e2e6b2b5f5c194266f6fa359759763fb87978
Maxim Pavlenko <maxim@MacBook-Air.local>
2020-03-10 18:20:56 +0200

    fix spinner

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -5,6 +5,11 @@ import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import Loader from 'react-loader-spinner';
 
 export const ImageGallery = ({ isLoading, gallery = [], onImageClick }) => {
+  const styleLoader = {
+    margin: '0px auto',
+    gridColumnStart: 1,
+    gridColumnEnd: 3,
+  };
   return (
     <>
       <ul className={styles.ImageGallery}>
@@ -22,14 +27,11 @@ export const ImageGallery = ({ isLoading, gallery = [], onImageClick }) => {
         })}
         {isLoading && (
           <Loader
-            style={{
-              margin: '0 auto',
-            }}
+            style={styleLoader}
             type="TailSpin"
             color="#3f51b5"
             height={100}
             width={100}
-            timeout={3000} //3 secs
           />
         )}
       </ul>
